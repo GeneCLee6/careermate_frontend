9ec9a987ea45f0723732462fa1bca2e9f6327fcd
Gene Lee <92206926+GeneCLee6@users.noreply.github.com>
2026-06-27 12:56:35 +0800

    Register page (#1)
    * register index completed
    
    * register index.css completed

---
+++ src/pages/register/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./index.css";
+
+const Register = () => {
+    return (
+        <div className="register-container">
+            <div className="register-card">
+                <h1>Careermate Register</h1>
+                <form className="submit-form">
+                    <div className="form-item-container">
+                        <div className="form-item">
+                            <label htmlFor="name" className="submit-form">
+                                Name
+                            </label>
+                            <input type="text" id="name" name="name" />
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="email" className="submit-form">
+                                Email
+                            </label>
+                            <input type="email" id="email" name="email" />
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="password" className="submit-form">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                            />
+                        </div>
+                        <div className="form-item">
+                            <label
+                                htmlFor="confirmPassword"
+                                className="submit-form"
+                            >
+                                Confirm Password
+                            </label>
+                            <input
+                                type="password"
+                                id="confirmPassword"
+                                name="confirmPassword"
+                            />
+                        </div>
+                        <button type="submit" className="submit">
+                            Register
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
